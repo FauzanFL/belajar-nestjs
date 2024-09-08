628c1a5a727002b8abee691d0e2d93b6e3132e50
Fauzan FL <fauzanarth@gmail.com>
2024-09-08 21:59:53 +0700

    fix missing imports

--- src/reservation/reservation.module.ts
+++ src/reservation/reservation.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ReservationService } from './reservation.service';
 import { ReservationController } from './reservation.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Reservation } from './entities/reservation.entity';
+import { RoomService } from 'src/room/room.service';
+import { UserService } from 'src/user/user.service';
+import { Room } from 'src/room/entities/room.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
   controllers: [ReservationController],
-  providers: [ReservationService],
+  providers: [ReservationService, RoomService, UserService],
+  imports: [TypeOrmModule.forFeature([Reservation, Room, User])]
 })
 export class ReservationModule {}
