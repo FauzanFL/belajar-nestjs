8fe93b0c8b99b3de48e66bacc732ee43cd5bb9bc
Fauzan FL <fauzanarth@gmail.com>
2024-09-09 21:26:28 +0700

    add decorator for schema

--- src/reservation/dto/create-reservation.dto.ts
+++ src/reservation/dto/create-reservation.dto.ts
@@ -1,11 +1,27 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateReservationDto {
+    @ApiProperty()
     date: Date;
+
+    @ApiProperty()
     duration: number;
+
+    @ApiProperty()
     phone: string;
+
+    @ApiProperty()
     total: number;
+
+    @ApiProperty()
     start: Date;
+
+    @ApiProperty()
     end: Date;
 
+    @ApiProperty()
     userId: number;
+
+    @ApiProperty()
     roomId: number;
 }
--- src/room/dto/create-room.dto.ts
+++ src/room/dto/create-room.dto.ts
@@ -1,9 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { RoomType } from "../entities/room.entity";
 
 export class CreateRoomDto {
+    @ApiProperty()
     name: string;
+
+    @ApiProperty()
     capacity: number;
+
+    @ApiProperty()
     type: RoomType;
+
+    @ApiProperty()
     price: number;
+
+    @ApiProperty()
     ready: boolean;
 }
